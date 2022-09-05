b6cbe7a594b5f58418a6c7de0170309e26f5326a
Benjamin Klarer <benjaminklarer@gmail.com>
2022-09-05 16:05:29 -0700

    setup state for notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
+import {useState, useEffect} from 'react'
 import '../App.css';
 import Search from './Search'
 import Navbar from './Navbar';
 import NoteContainer from './NoteContainer';
+import React from 'react';
 
 
 function App() {
+  
+  const [notes, setNotes] = useState([])
+  
+  useEffect(() => {
+    //fetch notes
+    
+  }, [])
+  
+  
   return (
     <div className="App">
       <Navbar/>
