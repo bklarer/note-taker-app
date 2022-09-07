ab6c9998447572dd1e6f187fe857c84f5240977f
Benjamin Klarer <benjaminklarer@gmail.com>
2022-09-06 22:48:57 -0700

    Add NavLinks to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,41 @@
-
-
-
+import {NavLink} from 'react-router-dom'
 
 function Navbar() {
 
 
     return (
-        <div>NavBar</div>
+        <div className="Navbar">
+            <NavLink
+                to="/"
+                exact
+                className="NavLink"
+                activeStyle={{
+                background: "white",
+                }}
+            >
+                Home
+            </NavLink>
+            <NavLink
+                to="/notes"
+                exact
+                className="NavLink"
+                activeStyle={{
+                background: "white",
+                }}
+            >
+                Notes
+            </NavLink>
+            <NavLink
+                to="/new"
+                exact
+                className="NavLink"
+                activeStyle={{
+                background: "white",
+                }}
+            >
+                New Note
+            </NavLink>          
+        </div>
     )
 
 }
