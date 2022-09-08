1d3b77cfc0a7b0354e9345b736729f48a5745848
Benjamin Klarer <benjaminklarer@gmail.com>
2022-09-07 22:16:11 -0700

    Adjust Styleing

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -10,6 +10,7 @@ function NoteForm() {
             <form className="form">
                 <input className="title" type="text" id="title" name="title" placeholder="Title"/>
                 <textarea className="note" type="text" id="note" name="note" placeholder="Type Here..."/>
+                <button className="submit" type="submit">Submit</button>
             </form>
         </div>
     )
