6a1c94da66b1d4f68aab066be78e1f08043fc659
Benjamin Klarer <benjaminklarer@gmail.com>
2022-09-07 23:05:04 -0700

    Adjust styling and structure of notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -12,11 +12,12 @@ function Note({title, content, id}) {
             <div className="note-content">
                 <h3>{title}</h3>
                 <p>{content}</p>
-            </div>
+            </div>            
             <div className="button-container">
-                <button><Link to={`/books/${id}`}>View</Link></button>
-                <button>Edit</button> {/*Need to figure out route when clicking edit through link and then patch*/}
-                <button>Delete</button> {/*Need to figure delete note with fetch and state*/}
+                
+                <button>✎</button> {/*Need to figure out route when clicking edit through link and then patch*/}
+                <button>♡</button> {/*Need to create state for making favorite*/}
+                <button>X</button> {/*Need to figure delete note with fetch and state*/}
             </div>
         </div>
     )
